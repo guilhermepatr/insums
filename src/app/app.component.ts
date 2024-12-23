b1a4ddf7b0eb6571e408dcb0024648904ddaa2d4
guilhermepatr <guilhermeps96@hotmail.com>
2024-12-23 11:20:55 -0300

    up readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './pags/login/login.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent,MainComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './pags/login/login.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'main', pathMatch: 'full' }, // Exemplo de redirecionamento para a página inicial
+    { path: 'login', component: LoginComponent }, // Rota para o componente de login
+    { path: 'main', component: MainComponent}
+    
+];
